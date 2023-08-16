e930b75fa548b31c52b5868d64c32e0cf60bab79
ayberktmn <ayberktemin@gmail.com>
2023-08-16 10:39:20 +0300

    persons fragment update

--- app/src/main/java/com/example/markakodportal/PersonsFragment.kt
+++ app/src/main/java/com/example/markakodportal/PersonsFragment.kt
@@ -16,6 +16,8 @@ class PersonsFragment : Fragment() {
 
     private var _binding: FragmentPersonsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var personsadapter: PersonsAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +40,7 @@ class PersonsFragment : Fragment() {
             .setDuration(1000)
             .start()
 
-        val profileList = createDummyData()
-
-        val profileAdapter = PersonsAdapter(profileList)
-        binding.rcylerPersons.layoutManager = LinearLayoutManager(requireContext())
-        binding.rcylerPersons.adapter = profileAdapter
+        initRecyclyer()
 
         val cikis = requireActivity().findViewById<ImageView>(R.id.imgback)
 
@@ -58,7 +56,6 @@ class PersonsFragment : Fragment() {
     private fun createDummyData(): List<Profile> {
         val dummyData = mutableListOf<Profile>()
 
-
         dummyData.add(Profile("Ayberk Temin","ayberkt@gmail.com", "Stajyer","https://media.licdn.com/dms/image/C4D03AQHGK0voeZK66A/profile-displayphoto-shrink_800_800/0/1662224359014?e=2147483647&v=beta&t=orno7saubT3HezSnCLUNFFC24iRrTtb95ReUztgJ_Z4"))
         dummyData.add(Profile( "Feyyaz Çakabey","feyyazcaka@gmail.com", "Ceo","https://media.licdn.com/dms/image/C4E03AQEl9rVOlv0UlA/profile-displayphoto-shrink_800_800/0/1600092651232?e=1695254400&v=beta&t=d_jcg0c2vt710CIcEsJ0YmRwTE3CGHaZsaEq_v0YHBw"))
         dummyData.add(Profile( "Yusuf Albayrak","yusufalbyrk@gmail.com", "Project Lead","https://media.licdn.com/dms/image/C5603AQFJGAtFz2dc-Q/profile-displayphoto-shrink_800_800/0/1546379045773?e=2147483647&v=beta&t=_seM317wOdlQZV4YEoZylDr1v_-YQROeSqchG8m0BV4"))
@@ -66,8 +63,15 @@ class PersonsFragment : Fragment() {
         dummyData.add(Profile( "Serkan Ateş","serkanats@gmail.com", "Senior Developer","https://media.licdn.com/dms/image/C4D03AQHc3SAt3-dK4g/profile-displayphoto-shrink_800_800/0/1632778488208?e=2147483647&v=beta&t=dCbjCE04oR-RG4CdLPQpMk71CdfIRY1TDWA4-Bl07o4"))
         dummyData.add(Profile( "Harun Hekimoğlu","harun@gmail.com", "Backend Developer","https://media.licdn.com/dms/image/C4E03AQEcH2MP8VtKwQ/profile-displayphoto-shrink_800_800/0/1627403275924?e=2147483647&v=beta&t=AcOiCZS-9agA8ZTuWP4V-Zf7vRWxa8Pa8shLyFrFPk4"))
 
-
         return dummyData
+    }
+
+    private fun initRecyclyer(){
+        val profileList = createDummyData()
+
+        personsadapter = PersonsAdapter(profileList)
+        binding.rcylerPersons.adapter = personsadapter
+        binding.rcylerPersons.layoutManager = LinearLayoutManager(requireContext())
     }
 
     override fun onDestroyView() {
